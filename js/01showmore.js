21c4d082e51661af69e13337ad5cf95ec1af69dc
Tanmay09516 <tanmay5tj@gmail.com>
2022-11-22 15:17:45 +0530

    js

--- js/01showmore.js
+++ js/01showmore.js
@@ -1,3 +1,48 @@
+// const showmore1 = document.getElementById("show-more-day1");
+// const showmore2 = document.getElementById("show-more-day2");
+// const schedulevisibility =
+//   document.getElementsByClassName("schedulevisibility");
+// const showbtn1 = document.getElementById("show-more1");
+// const showbtn2 = document.getElementById("show-more2");
+// showmore1.onclick = function () {
+//   //   schedulevisibility.classList.remove("invis");
+//   for (let i = 0; i < schedulevisibility.length; i++) {
+//     schedulevisibility[i].classList.toggle("invis");
+//   }
+//   if (showbtn1.innerHTML == "Show less" && showbtn2.innerHTML == "Show less") {
+//     showbtn1.innerHTML == "Show more";
+//     showbtn2.innerHTML == "Show more";
+//   } else {
+//     showbtn1.innerHTML == "Show less";
+//     showbtn2.innerHTML == "Show less";
+//   }
+//   // showmore1.classList.add("invis");
+//   // showmore2.classList.add("invis");
+// };
+
+// showmore2.onclick = function () {
+//   for (let i = 0; i < schedulevisibility.length; i++) {
+//     schedulevisibility[i].classList.toggle("invis");
+//     if (showbtn2.innerHTML == "Show less") {
+//       showbtn2.innerHTML == "Show more";
+//     } else {
+//       showbtn2.innerHTML == "Show less";
+//     }
+//   }
+//   if (showbtn1.innerHTML == "Show less" && showbtn2.innerHTML == "Show less") {
+//     showbtn1.innerHTML == "Show more";
+//     showbtn2.innerHTML == "Show more";
+//   } else if (
+//     showbtn1.innerHTML == "Show more" &&
+//     showbtn2.innerHTML == "Show more"
+//   ) {
+//     showbtn1.innerHTML == "Show less";
+//     showbtn2.innerHTML == "Show less";
+//   }
+//   // showmore1.classList.add("invis");
+//   // showmore2.classList.add("invis");
+// };
+
 const showmore1 = document.getElementById("show-more-day1");
 // const showmore11 = document.getElementById("show-more-day11");
 const showmore2 = document.getElementById("show-more-day2");
